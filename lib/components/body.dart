4d97d67a226b098a55143ae2611a49e3a50d4a43
gfadu <47917613+gfadu@users.noreply.github.com>
2020-10-15 21:46:03 +0530

    Update body.dart

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -11,6 +11,37 @@ class Body extends StatelessWidget {
         name: "Shayon Sarkar",
         email: "xyz@gmail.com",
       ),
+      InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 30,
+          ),
+          child: Row(
+            children: <Widget>[
+              Icon(
+                Icons.bookmark,
+              ),
+              SizedBox(width: 20,
+              ),
+              Text(
+                "Post",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+              Spacer(),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 16,
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      )
     ]
     );
   }
